bc66173da21abbfcc7de74753282ec78a0a80aab
lipen76 <44465117+lipen76@users.noreply.github.com>
2020-09-29 18:14:26 +0200

    fix: vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.direct = direct === false ? false : true;
+  }
+
+  ordA(a) {
+    return a.charCodeAt(0) - 65;
+  }
+
+  encrypt(word, keyword) {
+    if (!word || !keyword) {
+      throw new Error('Incorrect word or keyword');
+    }
+
+    const result = this.vigenere(word, keyword);
+
+    return this.direct
+      ? result
+      : result
+          .split('')
+          .reverse()
+          .join('');
+  }
+
+  decrypt(word, keyword) {
+    if (!word || !keyword) {
+      throw new Error('Incorrect word or keyword');
+    }
+
+    const result = this.vigenere(word, keyword, true);
+
+    return this.direct
+      ? result
+      : result
+          .split('')
+          .reverse()
+          .join('');
+  }
+
+  vigenere(text, key, decode) {
+    let i = 0;
+    let b;
+    const ordA = this.ordA;
+
+    key = key.toUpperCase();
+
+    return text.toUpperCase().replace(/[A-Z]/g, function(a) {
+      b = key[i++ % key.length];
+      return String.fromCharCode(
+        ((ordA(a) + (decode ? 26 - ordA(b) : ordA(b))) % 26) + 65,
+      );
+    });
   }
 }
 

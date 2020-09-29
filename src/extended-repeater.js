99d277424b1baa1fe58cf8a7e24005f79a71f41a
lipen76 <44465117+lipen76@users.noreply.github.com>
2020-09-29 18:11:06 +0200

    fix: update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(typeof str !== 'string') str = `${str}`
+    
+    if(!('repeatTimes' in options) || options.repeatTimes === undefined) {
+        options.repeatTimes = 1
+    }
+    if(!('addition' in options) || options.addition === undefined) {
+        options.addition = ""
+    }
+    if(!('additionRepeatTimes' in options) || options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 0
+    }
+    if(!('separator' in options) || options.separator === undefined) {
+        options.separator = "+"
+    }
+    if(!('additionSeparator' in options) || options.additionSeparator === undefined) {
+        options.additionSeparator = ""
+    }
+    let add
+    options.additionRepeatTimes === 0
+    ? add = options.addition
+    : add = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1).concat(options.addition)
+    
+    return str.concat(add).concat(options.separator).repeat(options.repeatTimes - 1).concat(str).concat(add)
 };
+  
   
